2ac6eba77b46a0947dcf994c2f50455f282d9891
zhanglingkang <lingkangzhangjava@163.com>
2014-11-05 12:03:41 +0800

    修正系统无法启动bug

--- Restaurant/js/app.js
+++ Restaurant/js/app.js
@@ -1,53 +1,55 @@
 /**
  * Created by Chris on 2/2/14.
  */
+;
+(function () {
+    var ppzRestaurant = angular.module('ppzRestaurant', ['ngRoute', 'ngCookies', 'ppzControllers', 'ppzDirectives', 'ppzServices', 'ui.bootstrap', 'angularFileUpload']);
 
-var ppzRestaurant = angular.module('ppzRestaurant', ['ngRoute', 'ngCookies', 'ppzControllers', 'ppzDirectives', 'ppzServices', 'ui.bootstrap', 'angularFileUpload']);
-
-ppzRestaurant.config(['$routeProvider', '$httpProvider', '$interpolateProvider',
-    function ($routeProvider, $httpProvider, $interpolateProvider) {
-        $httpProvider.interceptors.push(['$q', '$location', '$cookies', function ($q, $location, $cookies) {
-            return {
-                'response': function (response) {
-                    if ($location.path() !== '/login' && $cookies.token == 'null') {
-                        $location.path('/login');
+    ppzRestaurant.config(['$routeProvider', '$httpProvider', '$interpolateProvider',
+        function ($routeProvider, $httpProvider, $interpolateProvider) {
+            $httpProvider.interceptors.push(['$q', '$location', '$cookies', function ($q, $location, $cookies) {
+                return {
+                    'response': function (response) {
+                        if ($location.path() !== '/login' && $cookies.token == 'null') {
+                            $location.path('/login');
+                        }
+                        return response;
                     }
-                    return response;
-                }
-            };
-        }]);
+                };
+            }]);
 
-        $routeProvider.
-            when('/login', {
-                templateUrl: 'partials/login.html',
-                controller: 'loginController'
-            }).
-            when('/myRestaurants', {
-                templateUrl: 'partials/restaurantList.html',
-                controller: 'restaurantListController'
-            }).
-            when('/restaurant/:restaurantId', {
-                templateUrl: 'partials/restaurantDetails.html',
-                controller: 'restaurantDetailController'
-            }).
-            when('/waitinglist/:restaurantId', {
-                templateUrl: 'partials/waitinglist.html',
-                controller: 'waitingListController'
-            }).
-            when('/publicWaitList/:restaurantId', {
-                templateUrl: 'partials/publicWaitList.html',
-                controller: 'publicWaitListController'
-            }).
-            when('/printNumber/:unitId', {
-                templateUrl: 'partials/printNumber.html',
-                controller: 'printNumberController'
-            }).
-            when('/manageAccount', {
-                templateUrl: 'partials/manageAccount.html',
-                controller: 'manageAccountController'
-            }).
-            otherwise({
-                redirectTo: '/myRestaurants'
-            });
-    }
-]);
+            $routeProvider.
+                when('/login', {
+                    templateUrl: 'partials/login.html',
+                    controller: 'loginController'
+                }).
+                when('/myRestaurants', {
+                    templateUrl: 'partials/restaurantList.html',
+                    controller: 'restaurantListController'
+                }).
+                when('/restaurant/:restaurantId', {
+                    templateUrl: 'partials/restaurantDetails.html',
+                    controller: 'restaurantDetailController'
+                }).
+                when('/waitinglist/:restaurantId', {
+                    templateUrl: 'partials/waitinglist.html',
+                    controller: 'waitingListController'
+                }).
+                when('/publicWaitList/:restaurantId', {
+                    templateUrl: 'partials/publicWaitList.html',
+                    controller: 'publicWaitListController'
+                }).
+                when('/printNumber/:unitId', {
+                    templateUrl: 'partials/printNumber.html',
+                    controller: 'printNumberController'
+                }).
+                when('/manageAccount', {
+                    templateUrl: 'partials/manageAccount.html',
+                    controller: 'manageAccountController'
+                }).
+                otherwise({
+                    redirectTo: '/myRestaurants'
+                });
+        }
+    ]);
+}());
